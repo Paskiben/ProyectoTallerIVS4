f2149fc3167673e2532bab3cd72abc545914dca7
Benjamín <benrrixworks@gmail.com>
2022-12-07 12:40:24 -0300

    Tabla actualizada

--- pages/showschedule.js
+++ pages/showschedule.js
@@ -9,12 +9,13 @@ async function getSchedule(edificio){
     console.log(M[id]);
     console.log(M[id].horario);
     let periodos = M[id].horario;
-    for (let i=0; i<7; i++) for (let j=1; j<7; j++) document.getElementById(`${i*10 + j}`).innerHTML=periodos[j-1][i]!=0?`${periodos[j-1][i]}`:'';
+    for (let i=0; i<7; i++) for (let j=1; j<7; j++) document.getElementById(`${i*10 + j}`).innerHTML=periodos[j-1][i]!=0?`${periodos[j-1][i]}`:'-';
+    document.getElementById('indicadorDatosRecibidos').innerHTML="";
 }
 
 function readScheduleMatrix() {
     let lines = [(<HStack key="bar1" h="12.5%" w="100%" bgColor={"blue.400"} px="2%">
-            <Text w="6%" color="yellow">[P]</Text>
+            <Text w="6%" color="black" id='indicadorDatosRecibidos'>En espera.</Text>
             <Text key="Lunes" w="15.6%" textAlign="center" borderRight="2px">Lunes</Text>
             <Text key="Martes" w="15.6%" textAlign="center" borderRight="2px">Martes</Text>
             <Text key="Miercoles" w="15.6%" textAlign="center" borderRight="2px">Miercoles</Text>
@@ -23,6 +24,7 @@ function readScheduleMatrix() {
             <Text key="Sabado" w="15.6%" textAlign="center">Sabado</Text>
     </HStack>)]
 
+    let periodoRomano=['I','II','III','IV','V','VI','VII'];
 
     for (let i = 0; i < 7; i++)
         lines.push(
@@ -31,14 +33,14 @@ function readScheduleMatrix() {
         w="100%" 
         px="2%"
         bgColor={i&1?"gray.900":"black"} 
-        color={i&1?"gray.50":"orange"} >
-            <Text key={i*10 + 7} color="gray" id={"p"+i} w="6%" borderRight="2px">{i}</Text>
-            <Text key={i*10 + 1} id={i*10 + 1} w="16.6%" textAlign="center"></Text>
-            <Text key={i*10 + 2} id={i*10 + 2} w="16.6%" textAlign="center"></Text>
-            <Text key={i*10 + 3} id={i*10 + 3} w="16.6%" textAlign="center"></Text>
-            <Text key={i*10 + 4} id={i*10 + 4} w="16.6%" textAlign="center"></Text>
-            <Text key={i*10 + 5} id={i*10 + 5} w="16.6%" textAlign="center"></Text>
-            <Text key={i*10 + 6} id={i*10 + 6} w="16.6%" textAlign="center"></Text>
+        color={i&1?"gray.50":"gray.400"} >
+            <Text key={i*10 + 7} color="gray" id={"p"+i} w="6%" borderRight="2px">{periodoRomano[i]}</Text>
+            <Text key={i*10 + 1} id={i*10 + 1} w="16.6%" textAlign="center">-</Text>
+            <Text key={i*10 + 2} id={i*10 + 2} w="16.6%" textAlign="center">-</Text>
+            <Text key={i*10 + 3} id={i*10 + 3} w="16.6%" textAlign="center">-</Text>
+            <Text key={i*10 + 4} id={i*10 + 4} w="16.6%" textAlign="center">-</Text>
+            <Text key={i*10 + 5} id={i*10 + 5} w="16.6%" textAlign="center">-</Text>
+            <Text key={i*10 + 6} id={i*10 + 6} w="16.6%" textAlign="center">-</Text>
         </HStack>)
     
 
