92ee67b66327b1ee2d21b00fce29317efac5cf39
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2022-12-16 21:05:04 -0300

    boton

--- pages/9000.js
+++ pages/9000.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Flex, HStack, Text, Divider, Image, Grid, GridItem, Select, AspectRatio} from "@chakra-ui/react";
+import { Flex, HStack, Text, Divider, Image, Grid, GridItem, Select, AspectRatio } from "@chakra-ui/react";
 import { useState } from "react";
 import Script from "next/script";
 
-export default function Nueve(){
+export default function Nueve() {
     const [value, setValue] = useState("9101");
 
 
     return (
-        
+
         <>
             <Head>
                 <title>9000</title>
@@ -28,30 +28,30 @@ export default function Nueve(){
                 templateColumns="repeat(5,1fr)"
                 gap={4}
             >
-            <GridItem colSpan={1} height="full" border="0px" borderColor="white" >
-                    <Select id="salas" selectedoptioncolor="yellow" color="black" bg="white" colorScheme={"purple"} borderColor={"orange"} 
-                            onChange={(e) => {
+                <GridItem colSpan={1} height="full" border="0px" borderColor="white" >
+                    <Select id="salas" selectedoptioncolor="yellow" color="black" bg="white" colorScheme={"purple"} borderColor={"orange"}
+                        onChange={(e) => {
                             setValue(e.target.value);
-                                }}>
+                        }}>
                         <option value="9101" selected>9101</option>
                         <option value="9102">9102</option>
                         <option value="9103">9103</option>
                         <option value="9104">9104</option>
                     </Select>
-            </GridItem>
-            <GridItem colSpan={4} align="center" >
-                <Text color ="white"> Horario sala {value}</Text>
-                
-                <iframe
-                        src={"showschedule?sala="+value+"&edificio=9000"}
+                </GridItem>
+                <GridItem colSpan={4} align="center" >
+                    <Text color="white"> Horario sala {value}</Text>
+
+                    <iframe
+                        src={"showschedule?sala=" + value + "&edificio=9000"}
                         id="Schedule"
                         allowTransparency="true"
                         frameborder="0px"
                         width="90%"
                         height="500">
-                </iframe>
-                
-            </GridItem>
+                    </iframe>
+
+                </GridItem>
             </Grid>
             <HStack as="footer" h="15vh" bg="#161818" alignItems="center" border="0px">
                 <Image src="./images/KS.png" w="25vh" />
@@ -68,5 +68,5 @@ export default function Nueve(){
 
             </HStack>
         </>
-      );
+    );
 };
