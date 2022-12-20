73deecf410a899aae24f99b4f07d6f1cb139ea70
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2022-12-19 21:47:36 -0300

    iconos y botones

--- pages/9000.js
+++ pages/9000.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { Flex, HStack, Text, Divider, Image, Grid, GridItem, Select, AspectRatio } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router.js"
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { IoSave, IoOptionsOutline, IoClose, IoArrowUndoOutline } from "react-icons/io5";
+
 
 export async function Salas(edificio) {
     if (edificio == undefined) return;
@@ -34,11 +37,21 @@ export default function Nueve() {
                 <title>Edificio</title>
             </Head>
             <Flex bgGradient="linear(to-r, #e33e2e, #f7c21c)" h="5vh">
-                <Link href={"/test"}>
-                    <a className="volver">
-                        Volver
-                    </a>
-                </Link>
+                <ButtonGroup w={'120px'} h={'32px'} className="volver" bg={"#161818"} borderBottomRadius="10px" >
+                    <Button w={'120px'} h={'32px'} leftIcon={<IoArrowUndoOutline />} colorScheme='black' variant='solid'>
+                        <Link href={"/test"}>
+                            Volver
+                        </Link>
+                    </Button>
+                </ButtonGroup>
+
+
+                <ButtonGroup className="boton" variant='outline' spacing='3'>
+                    <Button leftIcon={<IoSave />} w={'90px'} h={'32px'} variant='solid' colorScheme='blue' >Guardar</Button>
+                    <Button leftIcon={<IoClose />} w={'90px'} h={'32px'} variant='solid' colorScheme={'red'}>Cancelar</Button>
+                    <Button leftIcon={<IoOptionsOutline />} colorScheme='green' variant='solid'>Modificar</Button>
+                </ButtonGroup>
+
             </Flex>
             <Grid
                 h="80vh"
@@ -50,16 +63,22 @@ export default function Nueve() {
                 templateColumns="repeat(5,1fr)"
                 gap={4}
             >
+
                 <GridItem colSpan={1} height="full" border="0px" borderColor="white" >
                     <Select onChange={iframegen} id="salas" selectedoptioncolor="yellow" color="black" bg="white" colorScheme={"purple"} borderColor={"orange"}>
                     </Select>
                 </GridItem>
+
                 <GridItem colSpan={4} align="center" >
                     <Text color="white"> Horario sala </Text>
                     <div id="frameplace"></div>
                 </GridItem>
             </Grid>
+
             <HStack as="footer" h="15vh" bg="#161818" alignItems="center" border="0px">
+                <Flex alignContent={'left'}>
+                    <Button leftIcon={<IoOptionsOutline />} bgGradient="linear(to-r, #e33e2e, #f7c21c)" >Modificar</Button>
+                </Flex>
                 <Image src="./images/KS.png" w="25vh" />
                 <Divider orientation="vertical" color="white"></Divider>
                 <Link
